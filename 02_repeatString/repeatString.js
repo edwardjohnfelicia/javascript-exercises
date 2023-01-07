9437ac7595fb2d29055780f2d9f1106d37480e08
Edward John Felicia <edwardjohnfelicia@gmail.com>
2023-01-08 05:32:21 +0800

    comment and compare solution from TOP of exercise 2 & 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -9,9 +9,18 @@ const repeatString = function(string, i) {
     } else {
         return 'ERROR'
     }
-    
-
 };
+
+//alternative solution from TOP
+
+// const repeatString = function(word, times) {
+//     if (times < 0) return 'ERROR';
+//     let string = '';
+//     for (let i = 0; i < times; i++) {
+//       string += word;
+//     }
+//     return string;
+//   };
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,18 @@
-const reverseString = function(str) {
+npconst reverseString = function(str) {
     let reversedString = "";
     for (let i=0; i<=(str.length-1); i++) {
         reversedString += str.charAt(str.length-(1+i))
     }
     return reversedString;
 };
+
+//alternative solution from TOP
+
+// const reverseString = function(string) {
+//     return string.split('').reverse().join('');
+//    };
+   
+//    module.exports = reverseString;
 
 // Do not edit below this line
 module.exports = reverseString;
