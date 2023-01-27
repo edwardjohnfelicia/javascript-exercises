ab2ce173c9a48f64437d54f28c81737862695994
Edward John Felicia <edwardjohnfelicia@gmail.com>
2023-01-27 15:47:11 +0800

    Do exercise number 8:calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+	a.forEach(item => {
+    total +=item;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let product = 1;
+	a.forEach(item => {
+    product *=item;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	// let result = Math.pow(a,b);
+  // return result;
+
+//alternate solution
+  let result = 1;
+  for (let i=0; i<b;i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a==0) return 1;
+	let result = 1;
+  for (let i=a; i>0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
