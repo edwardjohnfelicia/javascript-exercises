2eb4bd393c20b0a63206018aadabeb06d93a36f4
Edward John Felicia <edwardjohnfelicia@gmail.com>
2023-01-27 15:11:18 +0800

    Do exercise number 7:temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  celsius = 0;
+  celsius = (tempF-32)*(5/9)
+  roundedCelsius = celsius.toFixed(1);
+  return Number(roundedCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  fahrenheit = 0;
+  fahrenheit = (tempC*(9/5)+32)
+  roundedFahrenheit = fahrenheit.toFixed(1);
+  return Number(roundedFahrenheit);
 };
 
 // Do not edit below this line
