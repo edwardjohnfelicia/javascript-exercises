3763debfcb2fd6f96c1ca92d810b06f26a478175
Edward John Felicia <76100222+edwardjohnfelicia@users.noreply.github.com>
2023-03-20 18:52:42 +0800

    Do exercise number 9 and 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    let x;
+    let k=1;
+    let j=0;
+    if(Number(term) >= 1) {
+        for(let i=0; i<Number(term); i++) {
+            x = k + j;
+            k=j;
+            j=x;
+        }
+        return x;
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
