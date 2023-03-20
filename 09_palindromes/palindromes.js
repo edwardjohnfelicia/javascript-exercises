2108b3820a51fb7ac98e94f9dab66ac7468069e7
Edward John Felicia <76100222+edwardjohnfelicia@users.noreply.github.com>
2023-03-20 18:53:44 +0800

    Do exercise number 9 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-z0-9]/gi,"").toLowerCase();
+    // return str;
+    let reversedString = "";
+    for (let i=0; i<=(str.length-1); i++) {
+        reversedString += str.charAt(str.length-(1+i))
+    }
+    return str == reversedString;
 };
 
 // Do not edit below this line
