c66a65a7f29a7be1a4002c0732e9b03d0b50221e
Edward John Felicia <76100222+edwardjohnfelicia@users.noreply.github.com>
2023-03-20 23:03:35 +0800

    Do exercise #11 and #12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
 
+const getTheTitles = function(library) {
+    return library.map(book => book.title)
 };
 
 // Do not edit below this line
